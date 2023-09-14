389300792cfb62bba464600849de487a61c755a7
Andrew Lee <candrewlee14@gmail.com>
2023-09-13 20:28:42 -0600

    use bun in workflow

--- src/routes/player/[slug]/+page.ts
+++ src/routes/player/[slug]/+page.ts
@@ -2,8 +2,6 @@ import { error } from "@sveltejs/kit";
 import { characters } from "$lib/characters";
 import type {PageLoad} from './$types';
 
-export const prerender = true;
-
 export const load: PageLoad = ({ params }) => {
 
   const c = characters.find((character) => character.path === params.slug);
